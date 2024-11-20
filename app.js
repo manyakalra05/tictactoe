83dd744ea6247914193d5ef06821e5d689bef6bd
Manya Kalra <kalramanya212@gmail.com>
2024-11-20 06:00:16 +0530

    Update configuration

--- app.js
+++ app.js
@@ -109,3 +109,4 @@ resetbtn.addEventListener("click", resetGame);
 <!-- Update 2025-09-23T17:23:19+05:30 -->
 <!-- Update 2025-10-17T12:38:27+05:30 -->
 <!-- Update 2024-11-06T11:35:10+05:30 -->
+<!-- Update 2024-11-20T06:00:16+05:30 -->
