0aa68f98500780bd8972818fc8222029c07246a1
Manya Kalra <kalramanya212@gmail.com>
2025-04-09 10:58:38 +0530

    Add feature

--- app.js
+++ app.js
@@ -120,3 +120,4 @@ resetbtn.addEventListener("click", resetGame);
 <!-- Update 2025-03-02T11:35:21+05:30 -->
 <!-- Update 2025-03-20T05:49:28+05:30 -->
 <!-- Update 2025-03-31T08:19:31+05:30 -->
+<!-- Update 2025-04-09T10:58:38+05:30 -->
