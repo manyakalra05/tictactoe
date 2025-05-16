2da853f390908aa6d766adfb87e30ef97b460850
Manya Kalra <kalramanya212@gmail.com>
2025-05-16 16:12:52 +0530

    Optimize logic

--- app.js
+++ app.js
@@ -123,3 +123,4 @@ resetbtn.addEventListener("click", resetGame);
 <!-- Update 2025-04-09T10:58:38+05:30 -->
 <!-- Update 2025-04-14T11:55:41+05:30 -->
 <!-- Update 2025-05-13T09:53:47+05:30 -->
+<!-- Update 2025-05-16T16:12:52+05:30 -->
