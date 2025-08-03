05543153442744b4d57622b6985b50735ca65baa
Manya Kalra <kalramanya212@gmail.com>
2025-08-03 09:10:09 +0530

    Add tests

--- app.js
+++ app.js
@@ -104,3 +104,4 @@ resetbtn.addEventListener("click", resetGame);
 <!-- Update 2025-06-04T11:23:57+05:30 -->
 <!-- Update 2025-06-14T09:47:58+05:30 -->
 <!-- Update 2025-07-17T11:49:06+05:30 -->
+<!-- Update 2025-08-03T09:10:09+05:30 -->
