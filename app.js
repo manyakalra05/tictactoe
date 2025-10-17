e231276bf350511971f9a3ef97d4330330fe2916
Manya Kalra <kalramanya212@gmail.com>
2025-10-17 12:38:27 +0530

    Fix styling

--- app.js
+++ app.js
@@ -107,3 +107,4 @@ resetbtn.addEventListener("click", resetGame);
 <!-- Update 2025-08-03T09:10:09+05:30 -->
 <!-- Update 2025-09-13T08:26:15+05:30 -->
 <!-- Update 2025-09-23T17:23:19+05:30 -->
+<!-- Update 2025-10-17T12:38:27+05:30 -->
